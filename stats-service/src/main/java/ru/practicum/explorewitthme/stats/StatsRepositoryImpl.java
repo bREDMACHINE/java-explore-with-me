cddfc259c4db3b6a8e84813744ff2157d686d9bd
bREDMACHINE <yasenya1985@yandex.ru>
2022-10-30 23:23:55 +0300

    fix - fixed imports for tests

--- stats-service/src/main/java/ru/practicum/explorewitthme/stats/StatsRepositoryImpl.java
+++ stats-service/src/main/java/ru/practicum/explorewitthme/stats/StatsRepositoryImpl.java
@@ -1,10 +1,8 @@
 package ru.practicum.explorewitthme.stats;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
